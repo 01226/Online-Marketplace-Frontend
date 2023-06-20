929b214294287f1745492feaa46a7aba8a76cf16
Jia273 <114813337+01226@users.noreply.github.com>
2023-06-20 10:47:52 +0100

    Add existing project files to Git

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oa5s6wb1gFY30fIs5d7',
+        issuer: 'https://dev-23042745.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
